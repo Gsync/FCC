0f952fdfc4888e4ac2b58eb2928c140f66e4c170
Khuram Niaz <knshaikh@gmail.com>
2017-11-28 17:25:11 -0700

    scatterplot axis, text

--- d3Projects/d3Scatterplot/app.js
+++ d3Projects/d3Scatterplot/app.js
@@ -4,13 +4,13 @@ d3.json(url, function(error, data) {
     if (error) throw error;
 
     const yScale = d3.scaleLinear()
-                        .domain([0, d3.max(data, d=>d.Place)])
+                        .domain([d3.max(data, d=>d.Place)+1, 0])
                         .range([h-p, p]);
 
     
 
     const xScale = d3.scaleLinear()
-                        .domain([d3.min(data, d=>d.Seconds), d3.max(data, d=>d.Seconds)])
+        .domain([d3.max(data, d => d.Seconds)+10, d3.min(data, d => d.Seconds)])
                         .range([p, w-p]);
 
     const xAxis = d3.axisBottom(xScale);
@@ -36,7 +36,23 @@ d3.json(url, function(error, data) {
             .append('circle')
                 .attr('cx', d=>xScale(d.Seconds))
                 .attr('cy', d=>yScale(d.Place))
-                .attr('fill', 'blue')
+                .attr('fill', d => d.Doping ? 'red' : 'blue')
                 .attr('r', 5)
                 .attr('stroke', '#fff');
+
+        svg.append('text')
+            .attr('x', w/2)
+            .attr('y', h-p)
+            .text('Time in Seconds')
+            .style('text-anchor', 'middle')
+            .attr('dy', '2.5em');
+            
+        svg.append('text')
+            .attr('x', -h/2)
+            .attr('y', p)
+            .text('Ranking')
+            .style('text-anchor', 'middle')
+            .attr('dy', '-2em')
+            .attr('transform', 'rotate(-90)');
+
 })
